8e400418255da41025bad2a924437e466d62811b
RyanC1681 <40802485+RyanC1681@users.noreply.github.com>
2024-11-08 01:09:49 -0800

    Lab 6 progress

--- server/src/expenses/expense-endpoints.ts
+++ server/src/expenses/expense-endpoints.ts
@@ -1,3 +1,33 @@
+import { Database } from "sqlite";
+import { createExpenseServer, deleteExpense, getExpenses } from "./expense-utils";
+import { Request, Response } from 'express';
+
+export function createExpenseEndpoints(app: any, db: Database) {
+   // Create a new expense
+   app.post("/expenses", (req: Request, res: Response) => {
+
+       createExpenseServer(req, res, db);
+
+   });
+
+   // Delete an expense
+   app.delete("/expenses/:id", (req: Request, res: Response) => {
+
+       deleteExpense(req, res, db);
+
+   });
+
+   // Get all expenses
+   app.get("/expenses", (req: Request, res: Response) => {
+
+       getExpenses(req, res, db);
+
+   });
+
+}
+
+
+/*
 import { createExpenseServer, deleteExpense, getExpenses } from "./expense-utils";
 import { Request, Response } from 'express';
 
@@ -24,3 +54,4 @@ export function createExpenseEndpoints(app: any, expenses: any) {
     });
 
 }
+    /*
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,40 @@
-import { Request, Response } from "express";
+import { Response } from "express";
+import { createBudgetEndpoints } from "./budget/budget-endpoints";
+import { createExpenseEndpoints } from "./expenses/expense-endpoints";
+import { budget } from "./constants";
+import initDB from "./createTable";
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+// Start the server
+app.listen(port, () => {
+ console.log(`Server running at http://localhost:${port}`);
+});
+
+// Initialize the database and start the server
+(async () => {
+ const db = await initDB();
+
+ // Root endpoint to get test if the server is running
+ app.get("/", (res: Response) => {
+   res.send({ "data": "Hello, TypeScript Express!" });
+   res.status(200);
+ });
+
+ createExpenseEndpoints(app, db);
+
+ createBudgetEndpoints(app, budget);
+})();
+
+
+/*import { Request, Response } from "express";
 import { expenses } from "./constants";
 import { budget } from "./constants";
 
@@ -31,3 +67,4 @@ app.get("/", (req: Request, res: Response) => {
 
 createExpenseEndpoints(app, expenses);
 createBudgetEndpoints(app, budget);
+*/
